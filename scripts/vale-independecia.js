d72d6089413b788b37ff3c7ae0ab28f0f04bdcc4
diogovidall <diogo.vidal@sptech.school>
2023-05-14 12:51:19 -0300

    Arrumei alguns bugs do index

---
+++ scripts/vale-independecia.js
@@ -0,0 +1,13 @@
+var nomeCompleto = input-nome-completo.value;
+var nomeBarbeiro = input-nome-barbeiro.value;
+var dataCorte = diaRealizado.value;
+var bloco1 = block1.value;
+
+
+function validar() {
+if (nomeCompleto ==  'Diogo de Paiva Vidal'.toLowerCase && nomeBarbeiro == 'Marcelo'.toLowerCase) {
+alert("Anotando..")
+} else{
+    alert("Dados invalidos");
+}
+}
